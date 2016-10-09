a93bd7f81683ebc239b99e5c35d74afea5bebd80
Hertzog Bruno <bruno.hertzog@viacesi.fr>
2016-10-09 18:35:08 +0200

    ng repeat

--- index.controller.js
+++ index.controller.js
@@ -6,11 +6,38 @@ angular.module('notesApp')
 	   		console.log('Création de MainCtrl');
 	   		var ctrl = this;
 	   		// Attrbitut publique ---------------------------
-	   		ctrl.helloMsg = 'Bonjour';
 
+	   		ctrl.notes = [
+	   		{id:1, label:'Math', grade: 11, done: true},
+	   		{id:2, label:'Géographie', grade: 7, assignee: 'Albert', done: false},
+	   		{id:3, label:'Histoire', grade: 1, done: true},
+	   		{id:4, label:'Math', grade: 15, assignee: 'Michel', done: false},
+	   		];
 
-	   		// Fonctions publique ----------------------------
-	   		ctrl.changeMessage = function(newMsg) {
-	   			ctrl.helloMsg = newMsg;
+	   		ctrl.notes2 = 
+	   		{
+	   			brad:  {
+	   				id: 1,
+	   				label: 'First note',
+	   				done: false
+	   			},
+	   			Michel: {
+	   				id: 2,
+	   				label: 'Second note',
+	   				done: true
+	   			},
+	   			jason: {
+	   				id: 4,
+	   				label: 'Quatre note',
+	   				done: false
+	   			}
 	   		};
+	   		// Fonctions publique ----------------------------
+	   		ctrl.getNoteClass = function(status) {
+	   			return {
+	   				done: status,
+	   				pending: !status
+	   			};
+	   		};
+
 }]);
